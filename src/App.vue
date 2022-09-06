1cd9697cb2f55d5ce4a23fd9788af6500f0f83c1
Skaletti <gordey.korolev.2016@mail.ru>
2022-09-06 22:21:30 +0700

    my first vue cli project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s ease-out;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
